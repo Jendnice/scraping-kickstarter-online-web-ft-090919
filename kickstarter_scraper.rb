243c92910adc57005f8d7cdb5feff7a11f8def2b
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-28 15:49:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,21 +1,36 @@
 require 'nokogiri'
 require 'pry'
 
-
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text 
+# image link: project.css("div.project-thumbnail a img").attribute("src").value 
+# description: project.css("p.bbcard_blurb").text 
+# location: project.css("ul.project-meta span.location-name").text 
+# percent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
 def create_project_hash
-  
-  project = []
   
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
   
-  binding.pry 
+  projects = {}
+  
+  kickstarter.css("li.projcet.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text 
+    projects[title.to_sym] = {
+      :image_link => , 
+      :description => , 
+      :location => , 
+      :percent_funded => 
+    }
+  end 
+  
+  projects 
   
 end
 
-create_project_hash
+
 
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text 
